247d38a576cef62fa4037045327bcf829e2d79e6
Franco Díaz <fraediaz@icloud.com>
2026-04-21 23:37:34 -0400

    --

--- config/asgi.py
+++ config/asgi.py
@@ -13,11 +13,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import chat.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter(
 	{
